e9a41915daa82269e0791b818572ecc49da537b3
devmus <web3devmus@gmail.com>
2025-04-06 17:08:33 +0200

    auth

--- config/config.mjs
+++ config/config.mjs
@@ -1,1 +1,11 @@
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const fileName = fileURLToPath(import.meta.url);
+const dirname = path.dirname(fileName);
+global.__appdir = dirname;
+
+dotenv.config({ path: path.resolve(__appdir, "config.env") });
+
 export const APP_PORT = process.env.APP_PORT || 9995;
--- server.mjs
+++ server.mjs
@@ -1,7 +1,24 @@
 import express from "express";
 import colors from "colors";
-import { APP_PORT } from "./config/config.mjs";
+import { APP_PORT, authConfig } from "./config/config.mjs";
+import pkg from "express-openid-connect";
+import dotenv from "dotenv";
+dotenv.config();
+const { auth, requiresAuth } = pkg;
+
 const app = new express();
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(authConfig));
+
+// req.isAuthenticated is provided from the auth router
+app.get("/", (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
+
+app.get("/profile", requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
 
 app.listen(APP_PORT, () => {
   console.log(`Server is running on port: ${APP_PORT}`.green.underline);
